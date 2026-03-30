7797f89297e6701b58c8e6b4385bca720440bd96
nu-tree <knu9910@naver.com>
2026-03-30 17:05:56 +0900

    feat: 메뉴 default option추가, 팁탭 메뉴 기본값 적용가능하도록

--- src/app/_components/component-example.tsx
+++ src/app/_components/component-example.tsx
@@ -1,13 +1,14 @@
 'use client';
 import { cn } from '@/lib/utils';
 import { TiptapEditor, TiptapViewer } from '@/components/custom-ui/tiptap/core';
+import { Pxs } from '@/components/custom-ui/tiptap/plugin/tiptap-font-config/constants';
 
 type Props = React.HTMLAttributes<HTMLElement>;
 
 export const ComponentExample = ({ className }: Readonly<Props>) => {
   return (
     <div className={cn('', className)}>
-      <TiptapEditor keyId={'12'} />
+      <TiptapEditor keyId={'12'} defaultOptions={{ fontSize: Pxs.PX_16, fontFamily: '모노스페이스', bold: true }} />
       <h2 className="mb-3 mt-6 text-xl font-semibold">너는 viewer야 </h2>
       <TiptapViewer className="border p-4" keyId={'12'} />
 
